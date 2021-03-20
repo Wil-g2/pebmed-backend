9e522c93588fe4b2e39ed7d9abfd1deadc462063
Wil-g2 <wil-g2@hotmail.com>
2021-03-20 00:35:29 -0300

    chore: add users migration

---
+++ src/shared/infra/typeorm/migrations/1615941401771-CreateUsers.ts
@@ -0,0 +1,61 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateUsers1615941401771 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'full_name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'birth_date',
+            type: 'timestamp with time zone',
+            isNullable: true,
+          },
+          {
+            name: 'status',
+            type: 'integer',
+            default: 1,
+          },
+          {
+            name: 'role',
+            type: 'integer',
+            default: 1,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
