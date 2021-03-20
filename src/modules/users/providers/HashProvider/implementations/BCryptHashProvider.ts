988a9a46a930b0bc1654af9dc8a505ee94dc2a59
Wil-g2 <wil-g2@hotmail.com>
2021-03-20 02:41:52 -0300

    refactor: parse variable SALT in .env to number

--- src/modules/users/providers/HashProvider/implementations/BCryptHashProvider.ts
+++ src/modules/users/providers/HashProvider/implementations/BCryptHashProvider.ts
@@ -3,7 +3,7 @@ import IHashProvider from '../models/IHashProvider';
 
 class BCryptHashProvider implements IHashProvider {
   public async generateHash(payload: string): Promise<string> {
-    return hash(payload, process.env.SALT || 8);
+    return hash(payload, Number(process.env.SALT) || 8);
   }
 
   public async compareHash(payload: string, hashed: string): Promise<boolean> {
