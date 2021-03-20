ac98d00903eb0332891d7a719ab2eeaa436e59c8
Wil-g2 <wil-g2@hotmail.com>
2021-03-20 00:34:51 -0300

    chore: add Typeorm config file

---
+++ ormconfig.js
@@ -0,0 +1,15 @@
+require('dotenv/config');
+
+module.exports = {
+  type: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  entities: [process.env.DB_ENTITIES],
+  migrations: [process.env.DB_MIGRATIONS],
+  cli: {
+    migrationsDir: process.env.DB_MIGRATIONS_DIR,
+  },
+};
